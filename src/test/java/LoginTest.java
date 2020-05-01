91813730c2d4f92e83c034ed5060f2bcdde68d79
rxyjp2008 <rxyjp2008@hotmail.com>
2020-05-01 11:47:01 -0700

    create a new web-application using maven

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,79 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+ 
+
+public class LoginTest {
+	
+	private Login login;
+	
+	@Mock
+	private HttpServletRequest request;
+	
+	@Mock
+	private HttpServletResponse response;
+	
+	@Mock
+	private RequestDispatcher requestDispatcher;
+	
+	@Mock
+	private HttpSession session;
+	
+	@Before
+	public void setUp() throws IOException {
+		login = new Login();
+		MockitoAnnotations.initMocks(this);
+	}
+		
+	@Test
+	public void doPost_NonEmptyInput_GoToHomePage() throws ServletException, IOException {
+		when(request.getParameter("username")).thenReturn("admin");
+		when(request.getParameter("password")).thenReturn("Password123!");
+		when(request.getSession()).thenReturn(session);
+		when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
+		
+		login.doPost(request, response);
+		
+		verify(request).getRequestDispatcher("homepage.jsp");
+	}
+
+	@Test
+	public void doPost_OneEmptyInput_StaySamePage() throws ServletException, IOException {
+		when(request.getParameter("username")).thenReturn("admin");
+		when(request.getParameter("password")).thenReturn("");
+		when(request.getSession()).thenReturn(session);
+		when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
+		
+		login.doPost(request, response);
+		
+		verify(request).getRequestDispatcher("login.jsp");
+	}
+	
+	@Test
+	public void doPost_AllEmptyInput_StaySamePage() throws ServletException, IOException {
+		when(request.getParameter("username")).thenReturn("");
+		when(request.getParameter("password")).thenReturn("");
+		when(request.getSession()).thenReturn(session);
+		when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
+		
+		login.doPost(request, response);
+		
+		verify(request).getRequestDispatcher("login.jsp");
+	}
+}
---
+++ src/test/java/RegisterTest.java
@@ -0,0 +1,63 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+ 
+
+public class RegisterTest {
+	
+	private Register register;
+	
+	@Mock
+	private HttpServletRequest request;
+	
+	@Mock
+	private HttpServletResponse response;
+	
+	@Mock
+	private RequestDispatcher requestDispatcher;
+	
+	@Mock
+	private HttpSession session;
+	
+	@Before
+	public void setUp() throws IOException {
+		register = new Register();
+		MockitoAnnotations.initMocks(this);
+	}
+		
+	@Test
+	public void doPost_NonEmptyInput_GoToLoginPage() throws ServletException, IOException {
+		when(request.getParameter("first_name")).thenReturn("John");
+		when(request.getParameter("last_name")).thenReturn("Doe");
+		when(request.getParameter("username")).thenReturn("admin");
+		when(request.getParameter("password")).thenReturn("Password123!");
+		when(request.getParameter("address")).thenReturn("Howe Street");
+		when(request.getParameter("contact")).thenReturn("12345678");
+		when(request.getSession()).thenReturn(session);
+		when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
+		
+		register.doPost(request, response);
+		
+		verify(request).getRequestDispatcher("login.jsp");
+		verify(session).setAttribute("username", "admin");
+		verify(session).setAttribute("password", "Password123!");
+	}
+
+
+}
